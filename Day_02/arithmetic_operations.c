ada1d927de6e46ccd7dc27278ba73607f355fc83
harshxt27 <harshit.msj@Gmail.com>
2025-08-11 22:11:54 +0530

    Day_11
    C programs.

---
+++ Day_02/arithmetic_operations.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int first, second, add, subtract, multiply;
+    float divide;
+    
+ 
+    first = ;
+    second = y;
+
+    add        = first + second;
+    subtract = first - second;
+    multiply = first * second;
+    divide     = first / (float)second;   
+    printf("Sum = %d\n",add);
+    printf("Difference = %d\n",subtract);
+    printf("Multiplication = %d\n",multiply);
+    printf("Division = %.2f\n",divide);
+    
+}
---
+++ Day_02/hello.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    int num;
+    scanf("%d", &num);
+    printf("Hello, bananananaaaaa!");
+    printf("Hello, World!\n");
+    printf("You entered: %d\n", num);
+}
+// This program prints "Hello, World!" to the console.
